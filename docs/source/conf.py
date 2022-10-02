46a0fdef2eda4c7b09a17492417faa5db4bb3e28
Sara-Iftikhar <40801582+Sara-Iftikhar@users.noreply.github.com>
2022-10-03 00:04:54 +0900

    Add files via upload

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,1 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+
+project = 'arg_ml'
+copyright = '2022, SI'
+author = 'SI'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+'sphinx.ext.todo',
+'sphinx.ext.viewcode',
+'sphinx.ext.autodoc',
+'sphinx.ext.autosummary',
+'sphinx.ext.doctest',
+'sphinx.ext.intersphinx',
+'sphinx.ext.imgconverter',
+'sphinx_issues',
+'sphinx.ext.mathjax',
+'sphinx.ext.napoleon',
+'sphinx.ext.githubpages',
+"sphinx-prompt",
+"sphinx_gallery.gen_gallery",
+'sphinx.ext.ifconfig',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+sphinx_gallery_conf = {
+    'backreferences_dir': 'gen_modules/backreferences',
+    #'doc_module': ('sphinx_gallery', 'numpy'),
+    'reference_url': {
+        'sphinx_gallery': None,
+    },
+    'examples_dirs': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'scripts'),
+    'gallery_dirs': 'auto_examples',
+    'compress_images': ('images', 'thumbnails'),
+    'filename_pattern': '',
+    "ignore_pattern": 'utils',
+
+    'binder': {'org': 'sphinx-gallery',
+               'repo': 'sphinx-gallery.github.io',
+               'branch': 'master',
+               'binderhub_url': 'https://mybinder.org',
+               'dependencies': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.binder', 'requirements.txt'),
+               'notebooks_dir': 'notebooks',
+               'use_jupyter_lab': True,
+               },
+    #'show_memory': True,
+    #'junit': os.path.join('sphinx-gallery', 'junit-results.xml'),
+    # capture raw HTML or, if not present, __repr__ of last expression in
+    # each code block
+    'capture_repr': ('_repr_html_', '__repr__'),
+    'matplotlib_animations': True,
+    'image_srcset': ["2x"]
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. arg_ml documentation master file, created by
+   sphinx-quickstart on Sun Oct  2 20:32:19 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to arg_ml's!
+==================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   auto_examples/index
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
