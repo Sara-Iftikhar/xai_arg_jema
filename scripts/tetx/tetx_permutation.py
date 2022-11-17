f723ac5a5e08eeb93ce509363520a2f21bb323a1
Sara Iftikhar <sara.rwpk@gmail.com>
2022-11-17 18:01:01 +0900

    added feature importance for tetx

---
+++ scripts/tetx/tetx_permutation.py
@@ -0,0 +1,49 @@
+"""
+================
+sul1 permutation
+================
+"""
+
+from ai4water import Model
+
+from tetx_utils import get_fitted_model, tetx_data
+
+# %%
+model = get_fitted_model(Model)
+
+# %%
+
+x, y, input_features, output_features = tetx_data()
+
+#%%
+
+pimp = model.permutation_importance(x=x,
+                                    y=y,
+                                    n_repeats=100,
+                                    scoring="r2",
+                                    plot_type="boxplot")
+
+#%%
+
+pimp.plot_1d_pimp("bar_chart", sort=True)
+
+#%%
+
+pimp = model.permutation_importance(x=x,
+                                    y=y,
+                                    n_repeats=100,
+                                    scoring="nse",
+                                    plot_type="boxplot")
+
+#%%
+
+pimp.plot_1d_pimp("bar_chart", sort=True)
+
+
+#%%
+
+pimp = model.permutation_importance(x=x,
+                                    y=y,
+                                    n_repeats=100,
+                                    scoring="rmsle",
+                                    plot_type="boxplot")
